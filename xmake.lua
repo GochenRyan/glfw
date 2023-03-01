27c4c1833255f14a0b5e7b601fe06d0076f4360c
GochenRyan <rangaocheng@outlook.com>
2023-03-01 18:13:44 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,5 @@
-target("GLFW")
+target("libGLFW")
     set_kind("static")
-    set_languages("c")
-    -- set_targetdir("$(projectdir)/libs/GLFW")
     add_headerfiles(
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
@@ -9,30 +7,17 @@ target("GLFW")
     )
     add_files(
         "src/context.c",
-        "src/input.c",
         "src/init.c",
+        "src/input.c",
         "src/monitor.c",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/platform.c",
         "src/vulkan.c",
         "src/window.c"
     )
-
-    if is_plat("linux") then
-        add_defines(
-            "_GLFW_X11"
-        )
-        add_files(
-            "src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-        )
-    end
 
     if is_plat("windows") then
         add_defines(
@@ -41,14 +26,15 @@ target("GLFW")
         )
         add_files(
             "src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+            "src/win32_joystick.c",
+            "src/win32_module.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         )
+        add_syslinks("opengl32", "gdi32", "user32", "kernel32", "shell32")
     end
-    set_group("Dependencies")
